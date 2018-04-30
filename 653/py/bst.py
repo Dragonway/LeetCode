eac7360e70b852444a7eb660354bf5cf366fa08a
Ivan Leonov <ivan.leonov.d@yandex.com>
2018-04-30 16:53:51 +0300

    Implement simple BST for #653

---
+++ 653/py/bst.py
@@ -0,0 +1,40 @@
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, x: int):
+        self.val: int = x
+        self.left: Optional[TreeNode] = None
+        self.right: Optional[TreeNode] = None
+
+
+class BinarySearchTree:
+    def __init__(self, elements: Optional[List[int]]):
+        self.root: Optional[TreeNode] = None
+        self.size = 0
+
+        for elem in elements:
+            self.add(elem)
+
+    def add(self, element: int) -> None:
+        if self.root is None:
+            self.root = TreeNode(element)
+            return
+
+        node = self.root
+
+        while True:
+            if element > node.val:
+                if node.right is None:
+                    node.right = TreeNode(element)
+                    return
+
+                node = node.right
+            elif element < node.val:
+                if node.left is None:
+                    node.left = TreeNode(element)
+                    return
+
+                node = node.left
+            else:
+                return
