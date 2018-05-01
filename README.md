75a5345272e28012e2057f2635fed538a91a03e6
Ivan Leonov <ivan.leonov.d@yandex.com>
2018-05-01 13:48:30 +0300

    #21 'Merge Two Sorted Lists' solution (python)

--- README.md
+++ README.md
@@ -5,5 +5,6 @@
 |  1  | [Two Sum](https://leetcode.com/problems/two-sum/) | [Python](./py/two_sum1.py) | easy |
 |  14 | [Longest Common Prefix](https://leetcode.com/problems/longest-common-prefix) | [Python](./py/longest_common_prefix.py) | easy |
 |  20 | [Valid Parentheses](https://leetcode.com/problems/valid-parentheses) | [Python](./py/valid_parentheses.py) | easy |
+|  21 | [Merge Two Sorted Lists](https://leetcode.com/problems/merge-two-sorted-lists) | [Python](./py/merge_two_sorted_lists.py) | easy |
 | 167 | [Two Sum II - Input array is sorted](https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/) | [Python](./py/two_sum2.py) | easy |
 | 653 | [Two Sum IV - Input is a BST](https://leetcode.com/problems/two-sum-iv-input-is-a-bst) | [Python](./py/two_sum4.py) | easy |
---
+++ py/merge_two_sorted_lists.py
@@ -0,0 +1,38 @@
+# Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+#
+# Example:
+#
+# Input: 1->2->4, 1->3->4
+# Output: 1->1->2->3->4->4
+
+
+from py.collections.list import ListNode
+
+
+class Solution:
+    def merge_two_lists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if l1 is None:
+            return l2
+
+        if l2 is None:
+            return l1
+
+        if l1.val < l2.val:
+            l0 = l1
+            l1 = l1.next
+        else:
+            l0 = l2
+            l2 = l2.next
+
+        cur = l0
+        while l1 is not None and l2 is not None:
+            if l1.val < l2.val:
+                cur.next = l1
+                l1 = l1.next
+            else:
+                cur.next = l2
+                l2 = l2.next
+            cur = cur.next
+
+        cur.next = l1 if l1 is not None else l2
+        return l0
