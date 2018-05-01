f66b843f328d4509bb1c3a882d90e309d98b7594
Ivan Leonov <ivan.leonov.d@yandex.com>
2018-05-01 14:55:26 +0300

    Add test for #88 'Merge Sorted Array' problem

--- py/tests/test_merge_sorted.py
+++ py/tests/test_merge_sorted.py
@@ -18,3 +18,15 @@ class TestMergeSorted(unittest.TestCase):
         test(List([1, 2, 3]).root,  List([]).root,              result=List([1, 2, 3]).root)
         test(List([]).root,         List([1, 2, 3]).root,       result=List([1, 2, 3]).root)
         test(List([]).root,         List([]).root,              result=List([]).root)
+
+    @test(merge2array.Solution.merge)
+    def test_merge_two_sorted_arrays(self) -> None:
+        test([1, 2, 4, 0, 0, 0],    3,  [1, 3, 4],      3,  result=[1, 1, 2, 3, 4, 4])
+        test([1, 1, 1, 1, 1, 1],    3,  [1, 1, 1],      3,  result=[1, 1, 1, 1, 1, 1])
+        test([-2, 0, 2, -1, 0, 1],  3,  [-4, -3, -1],   3,  result=[-4, -3, -2, -1, 0, 2])
+        test([1, 2, 3, 3, 2, 1],    3,  [4, 5, 6],      3,  result=[1, 2, 3, 4, 5, 6])
+        test([4, 5, 6, 0, 0, 0],    3,  [1, 2, 3],      3,  result=[1, 2, 3, 4, 5, 6])
+        test([4, 5, 6, 7, 8],       3,  [1, 2],         2,  result=[1, 2, 4, 5, 6])
+        test([1, 2, 3],             3,  [],             0,  result=[1, 2, 3])
+        test([0, 0, 0],             0,  [1, 2, 3],      3,  result=[1, 2, 3])
+        test([],                    0,  [],             0,  result=[])
