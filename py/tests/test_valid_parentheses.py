da2cc444972a487c131005b8e5bb65b82714301b
Ivan Leonov <ivan.leonov.d@yandex.com>
2018-05-01 11:42:33 +0300

    Add test for #20 'Valid Parentheses' problem

---
+++ py/tests/test_valid_parentheses.py
@@ -0,0 +1,17 @@
+import unittest
+from py.tests.utils import test
+from py import valid_parentheses as vp
+
+
+class TestValidParentheses(unittest.TestCase):
+
+    @test(vp.Solution.is_valid)
+    def test_valid_parentheses(self) -> None:
+        test("()", result=True)
+        test("()[]{}", result=True)
+        test("(]", result=False)
+        test("([)]", result=False)
+        test("{[]}", result=True)
+        test("", result=True)
+        test(")()", result=False)
+        test("(())((())))", result=False)
