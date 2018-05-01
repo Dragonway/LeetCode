f444acf9132a1c61657febb7431219034edee406
Ivan Leonov <ivan.leonov.d@yandex.com>
2018-05-01 18:33:41 +0300

    Add test for #27 'Remove Element' problem

--- py/tests/test_remove_elements.py
+++ py/tests/test_remove_elements.py
@@ -1,6 +1,7 @@
 import unittest
 from py.tests.utils import test
 from py import remove_duplicates_from_sorted_array as rdfsa
+from py import remove_element as re
 
 
 class TestRemoveElements(unittest.TestCase):
@@ -12,3 +13,13 @@ class TestRemoveElements(unittest.TestCase):
         test([1, 1, 1, 1],          result=[1])
         test([0],                   result=[0])
         test([],                    result=[])
+
+    @test(re.Solution.remove_element)
+    def test_remove_element(self) -> None:
+        test([3, 2, 2, 3],              3,  result=[2, 2])
+        test([0, 1, 2, 2, 3, 0, 4, 2],  2,  result=[0, 1, 4, 0, 3])
+        test([1, 1, 1],                 1,  result=[])
+        test([3, 4],                    0,  result=[3, 4])
+        test([1],                       0,  result=[1])
+        test([0],                       0,  result=[])
+        test([],                        0,  result=[])
