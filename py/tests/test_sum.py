01bed88327b8c4a3174ea2173c6b4549df932f6d
Ivan Leonov <ivan.leonov.d@yandex.com>
2018-05-02 18:14:37 +0300

    Add test for #2 'Add Two Numbers' problem

--- py/tests/test_sum.py
+++ py/tests/test_sum.py
@@ -1,6 +1,8 @@
 import unittest
 from py.tests.utils import test
+from py.collections.list import List
 from py import array_partition1 as ap1
+from py import add_two_numbers as add2nums
 
 
 class TestSum(unittest.TestCase):
@@ -12,3 +14,12 @@ class TestSum(unittest.TestCase):
         test([0, 0, 0, 0, 0, 0],    result=0)
         test([4, 2, -2, -4],        result=-2)
         test([1, -1, -1, 1],        result=0)
+
+    @test(add2nums.Solution.add_two_numbers)
+    def test_add_two_numbers(self) -> None:
+        test(List([2, 4, 3]).root,  List([5, 6, 4]).root,       result=List([7, 0, 8]).root)
+        test(List([2, 4, 3]).root,  List([0]).root,             result=List([2, 4, 3]).root)
+        test(List([0]).root,        List([5, 6, 4]).root,       result=List([5, 6, 4]).root)
+        test(List([1, 1, 1]).root,  List([2, 3]).root,          result=List([3, 4, 1]).root)
+        test(List([1, 1, 1]).root,  List([9, 8, 8, 9]).root,    result=List([0, 0, 0, 0, 1]).root)
+        test(List([5]).root,        List([6]).root,             result=List([1, 1]).root)
