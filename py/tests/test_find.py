80a3568a76db4f6ff3a3a6b3a3d1a2499765cb2b
Ivan Leonov <ivan.leonov.d@yandex.com>
2018-05-02 10:17:40 +0300

    Missed annotation

--- py/tests/test_find.py
+++ py/tests/test_find.py
@@ -6,7 +6,7 @@ from py import find_disappeared_numbers as fdn
 class TestFind(unittest.TestCase):
 
     @test(fdn.Solution.find_disappeared_numbers)
-    def test_find_disappeared_numbers(self):
+    def test_find_disappeared_numbers(self) -> None:
         test([4, 3, 2, 7, 8, 2, 3, 1],  result=[5, 6])
         test([8, 8, 8, 8, 8, 8, 8, 8],  result=[1, 2, 3, 4, 5, 6, 7])
         test([4, 2, 5, 7, 8, 6, 3, 1],  result=[])
