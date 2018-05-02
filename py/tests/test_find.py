621d1494ac56f1c1986267cb2b85e4775da83df1
Ivan Leonov <ivan.leonov.d@yandex.com>
2018-05-02 11:48:05 +0300

    Add test for #695 'Max Area of Island' problem

--- py/tests/test_find.py
+++ py/tests/test_find.py
@@ -1,6 +1,7 @@
 import unittest
 from py.tests.utils import test
 from py import find_disappeared_numbers as fdn
+from py import max_area_of_island as maoi
 
 
 class TestFind(unittest.TestCase):
@@ -15,3 +16,49 @@ class TestFind(unittest.TestCase):
         test([1, 1],                    result=[2])
         test([1],                       result=[])
         test([],                        result=[])
+
+    @test(maoi.Solution.max_area_of_island)
+    def test_max_area_of_island(self) -> None:
+        test([[0, 0, 0, 0, 0]],     result=0)
+
+        test([[0, 0, 1, 1, 1],
+              [0, 0, 0, 0, 0]],     result=3)
+
+        test([[0, 0, 1, 0, 0],
+              [1, 1, 0, 0, 0]],     result=2)
+
+        test([[0, 0, 1, 0, 0],
+              [1, 0, 1, 1, 0],
+              [1, 1, 0, 0, 0]],     result=3)
+
+        test([[1, 1, 0, 0, 0],
+              [1, 1, 0, 0, 1],
+              [0, 0, 1, 1, 0],
+              [1, 0, 0, 1, 0],
+              [0, 0, 0, 0, 0]],     result=4)
+
+        test([[0, 0, 1, 0, 1],
+              [1, 1, 1, 0, 1],
+              [1, 0, 0, 1, 1],
+              [1, 0, 0, 1, 0],
+              [1, 1, 1, 1, 0]],     result=15)
+
+        test([[0, 0, 1, 0, 1],
+              [1, 1, 1, 1, 1],
+              [1, 0, 1, 1, 1],
+              [1, 0, 0, 1, 0],
+              [1, 1, 1, 1, 0]],     result=17)
+
+        test([[1, 1, 1, 1, 1],
+              [1, 1, 1, 1, 1],
+              [1, 1, 1, 1, 1],
+              [1, 1, 1, 1, 1],
+              [1, 1, 1, 1, 1]],     result=25)
+
+        test([[1, 0, 1, 0, 1],
+              [0, 1, 0, 1, 0],
+              [1, 0, 1, 0, 1],
+              [0, 1, 0, 1, 0],
+              [1, 0, 1, 0, 1]], result=1)
+
+        test([[1]],                 result=1)
